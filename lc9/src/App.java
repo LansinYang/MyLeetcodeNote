eaab9602115bf352050d007dd465add5a9c72ef2
LansinYang <78700577+LansinYang@users.noreply.github.com>
2022-03-22 16:56:10 -0500

    Update App.java

--- lc9/src/App.java
+++ lc9/src/App.java
@@ -3,4 +3,17 @@ public class App {
         int i = 121;
         Solution(i);
     }
+    class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x %10 == 0 && x!=0)) return false;
+           int reverNum = 0;
+           while (x > reverNum){
+               reverNum = reverNum *10+x%10;
+               System.out.println("rever:" + reverNum);
+               x /= 10;
+               System.out.println(x);
+           }
+        return (reverNum == x)||(reverNum/10==x);
+    }
+}
 }
