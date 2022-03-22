79f2d0948d44a5fc2470e0a581d5bacbf73c438f
Lanxin <lanxin.yang@austin.utexas.edu>
2022-03-22 17:06:52 -0500

    Add src

--- lc9/src/App.java
+++ lc9/src/App.java
@@ -1,19 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         int i = 121;
-        Solution(i);
+        Solution j = new Solution();
+        System.out.println( j.isPalindrome(i));
     }
-    class Solution {
-    public boolean isPalindrome(int x) {
-        if(x<0 || (x %10 == 0 && x!=0)) return false;
-           int reverNum = 0;
-           while (x > reverNum){
-               reverNum = reverNum *10+x%10;
-               System.out.println("rever:" + reverNum);
-               x /= 10;
-               System.out.println(x);
-           }
-        return (reverNum == x)||(reverNum/10==x);
-    }
-}
 }
---
+++ lc9/src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x %10 == 0 && x!=0)) return false;
+           int reverNum = 0;
+           while (x > reverNum){
+               reverNum = reverNum *10+x%10;
+               System.out.println("rever:" + reverNum);
+               x /= 10;
+               System.out.println(x);
+           }
+        return (reverNum == x)||(reverNum/10==x);
+    }
+}
