861d6286f4bc17fd633d70a84fb3a40afeebe0ef
LansinYang <78700577+LansinYang@users.noreply.github.com>
2022-03-22 19:29:38 -0500

    Update Solution.java

--- lc12/src/Solution.java
+++ lc12/src/Solution.java
@@ -1,3 +1,14 @@
 public class Solution {
-    
+    public String intToRoman(int num) {
+        int[] roman = new int[]{1,5,10,50,100,500,1000};
+        
+        for(int i= roman.length-1; i >= 0;i--){
+            if(roman[i]>num){
+                continue;
+            }
+            while(res != 0){
+                res = res * roman[i+1]+num%roman[i];
+            }
+        }
+    }
 }
