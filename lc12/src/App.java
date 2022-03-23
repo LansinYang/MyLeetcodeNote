2ffa9d462a9841a1b72895c717a46c1587096e4f
Lanxin <lanxin.yang@austin.utexas.edu>
2022-03-22 19:56:34 -0500

    Add Solution

--- lc12/src/App.java
+++ lc12/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Solution s = new Solution();
+        System.out.println(s.intToRoman(3999));
     }
 }
--- lc12/src/Solution.java
+++ lc12/src/Solution.java
@@ -1,14 +1,23 @@
 public class Solution {
     public String intToRoman(int num) {
         int[] roman = new int[]{1,5,10,50,100,500,1000};
-        
+        char[] letter = new char[]{'I','V','X','L','C','D','M'};
+        String st = "I";
         for(int i= roman.length-1; i >= 0;i--){
             if(roman[i]>num){
                 continue;
             }
-            while(res != 0){
-                res = res * roman[i+1]+num%roman[i];
+            while(num > 0){
+                int m = num/roman[i];
+                num = num - m*roman[i];
+                while(m != 0){
+                    st +=letter[i];
+                    m--;
+                }
+                i--;
             }
+            break;
         }
+        return st.substring(1);
     }
 }
