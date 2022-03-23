1deef10ee32160c5f9b56b5a022582075e4d8e5a
LansinYang <78700577+LansinYang@users.noreply.github.com>
2022-03-22 19:14:26 -0500

    Delete lc11 directory

--- lc11/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        int[] height = new int[]{1,1};
-        Solution sl = new Solution();
-        System.out.println(sl.maxArea(height));
-    }
-}
