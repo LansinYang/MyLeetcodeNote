cc103f47155d5ca511b78689ddb247e50ec96e89
Lanxin <lanxin.yang@austin.utexas.edu>
2022-03-24 16:11:09 -0500

    Correct Solution

--- lc14/src/App.java
+++ lc14/src/App.java
@@ -2,6 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         Solution sl = new Solution();
         String[] strs = new String[]{"a"};
-        System.out.println(sl.longestCommonPrefix(strs));
     }
 }
--- lc14/src/Solution.java
+++ lc14/src/Solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 1 || strs.length == 0) return "";
+        if (strs.length == 0) return "";
         for (int j = 0; j < strs[0].length(); ++j) {
             for (int i = 0; i < strs.length; ++i) {
                 if (j >= strs[i].length() || strs[i].charAt(j) != strs[0].charAt(j)) {
