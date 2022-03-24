9dce5ab9128ef67f5dc9aae422bdcf2ada2470bc
Lanxin <lanxin.yang@austin.utexas.edu>
2022-03-24 15:56:03 -0500

    Add Solution

---
+++ lc14/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution sl = new Solution();
+        String[] strs = new String[]{"floght","flow","flower"};
+        System.out.println(sl.longestCommonPrefix(strs));
+    }
+}
---
+++ lc14/src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs){
+        if(strs.length == 0) return "";
+        String str = new String();
+        for(int i = 0; i < strs[0].length(); i++){
+            for(int j = 1; j < strs.length; j++){
+                if(i > strs[j].length()-1|| (strs[0].charAt(i)!=strs[j].charAt(i))){
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return str;
+    }
+}
