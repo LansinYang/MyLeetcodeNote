0c3af5a491314e546759e085674ca81035eacd35
Lanxin <lanxin.yang@austin.utexas.edu>
2022-03-24 16:05:59 -0500

    Add Solution

--- lc14/src/App.java
+++ lc14/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Solution sl = new Solution();
-        String[] strs = new String[]{"floght","flow","flower"};
+        String[] strs = new String[]{"a"};
         System.out.println(sl.longestCommonPrefix(strs));
     }
 }
--- lc14/src/Solution.java
+++ lc14/src/Solution.java
@@ -1,14 +1,15 @@
 public class Solution {
-    public String longestCommonPrefix(String[] strs){
-        if(strs.length == 0) return "";
-        String str = new String();
-        for(int i = 0; i < strs[0].length(); i++){
-            for(int j = 1; j < strs.length; j++){
-                if(i > strs[j].length()-1|| (strs[0].charAt(i)!=strs[j].charAt(i))){
-                    return strs[0].substring(0, i);
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 1 || strs.length == 0) return "";
+        for (int j = 0; j < strs[0].length(); ++j) {
+            for (int i = 0; i < strs.length; ++i) {
+                if (j >= strs[i].length() || strs[i].charAt(j) != strs[0].charAt(j)) {
+                    return strs[i].substring(0, j); 
+                }else if(strs[i].length() == 0){
+                    return "";
                 }
             }
         }
-        return str;
+        return strs[0];
     }
 }
